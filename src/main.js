48fa945908be2e077bd409c5f760492152695964
ogriffithjones <ogriffithjones@gmail.com>
2020-08-18 10:58:10 +1200

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+// Pages
+import Designers from '@/components/designers/index'
+import Home from '@/components/home/index'
+import About from '@/components/about/index'
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },{
+    path: '/designers',
+    component: Designers
+  },{
+    path: '/about',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  routes: routes,
+  router,
+  mode: "history"
+})
 
 Vue.config.productionTip = false
 
