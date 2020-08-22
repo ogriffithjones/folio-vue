c75b66065fc760bb54de214ecd5cacea592670ca
Chris Coulbeck <chriis.coulbeck@gmail.com>
2020-08-22 12:00:07 +1200

    Set up project card component to loop through each project and applied styling

--- src/components/home/designer.section.vue
+++ src/components/home/designer.section.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <ProjectCard />
+  <div class="projects">
+    <ProjectCard
+      v-for="(project, index) in projects"
+      v-bind:key="index"
+      :project="project"
+    />
   </div>
 </template>
 
@@ -20,28 +24,28 @@ export default {
           title: "Project title",
           body:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          img: "https://picsum.photos/seed/picsum/100",
+          img: "https://picsum.photos/seed/picsum/1000"
         },
         {
           id: 2,
           title: "Project title",
           body:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          img: "https://picsum.photos/seed/picsum/100",
+          img: "https://picsum.photos/seed/picsum/1000"
         },
         {
           id: 3,
           title: "Project title",
           body:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          img: "https://picsum.photos/seed/picsum/100",
+          img: "https://picsum.photos/seed/picsum/1000"
         },
         {
           id: 4,
           title: "Project title",
           body:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          img: "https://picsum.photos/seed/picsum/100",
+          img: "https://picsum.photos/seed/picsum/1000"
         },
       ],
     };
@@ -51,4 +55,8 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
+
+.projects {
+    display: flex;
+}
 </style>
--- src/components/home/project.component.vue
+++ src/components/home/project.component.vue
@@ -1,19 +1,59 @@
 <template>
-  <div>
-      <h1>Hello World</h1>
-      <p></p>
-      <img src="" alt="">
+  <div class="project">
+      <h1>{{project.title}}</h1>
+      <p>{{project.body}}</p>
+      <img v-bind:src="project.img" alt="">
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Project-Component'
+    name: 'Project-Component',
+    props: ['project']
 
 }
 </script>
 
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.project {
+  //Size
+  width: 350px;
+  //Spacing
+  margin: 15px;
+  padding-right: 30px;
+  border-right: solid 1px $seccondary-sun_burn;
+}
+
+h1 {
+  //Font
+  @include heading-sub;
+  //Spacing
+  padding-bottom: 20px;
+}
+
+p {
+  //Color
+  color: $seccondary-sun_burn;
+  //Font
+  @include paragraph-small;
+  //Spacing
+  padding-bottom: 20px;
+  
+}
+
+img {
+  //Size
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
 
 </style>
