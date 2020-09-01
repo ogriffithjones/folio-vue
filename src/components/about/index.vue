7201359567404b1578a1f72993da916603e0c64d
himanshu745 <himanshurajpal33@gmail.com>
2020-09-02 10:00:00 +1200

    about-us page up to date

---
+++ src/components/about/index.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+      <Hero/>
+
+      <Content/>
+  </div>
+</template>
+
+<script>
+import Content from './content.section'
+import Hero from './hero.section'
+export default {
+    name:'Home',
+    components: {
+        'Content': Content,
+        'Hero': Hero
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_variables.scss";
+</style>
